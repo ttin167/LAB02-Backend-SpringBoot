379135ea8f192f350a5a3562c482a911a39af478
ttin167 <150877853+ttin167@users.noreply.github.com>
2025-03-06 21:42:03 +0700

    Add files via upload

---
+++ src/main/java/controllers/TodoController.java
@@ -0,0 +1,42 @@
+package org.example.lab02.controllers;
+
+import org.example.lab02.pojos.Todo;
+import org.example.lab02.services.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public Collection<Todo> getTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoService.createTodo(todo);
+    }
+
+    @GetMapping("/{id}")
+    public Todo getTodoById(@PathVariable Long id) {
+        return todoService.getTodoById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Todo updateTodo(@PathVariable Long id, @RequestBody Todo updatedTodo) {
+        return todoService.updateTodo(id, updatedTodo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable Long id) {
+        todoService.deleteTodo(id);
+    }
+}
