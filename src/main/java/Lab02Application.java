50379d5fb8bec380f30c3d3489539d0463cd1ccb
ttin167 <150877853+ttin167@users.noreply.github.com>
2025-03-06 21:53:35 +0700

    Add files via upload

---
+++ src/main/java/Lab02Application.java
@@ -0,0 +1,10 @@
+package org.example.lab02;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab02Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Lab02Application.class, args);
+    }
+}
